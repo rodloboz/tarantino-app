f9eee0773b490f96dd6abdf83985f1653dceaac5
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-02-02 17:19:06 +0000

    configures element ui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-navbar></app-navbar>
+    <page-container></page-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavbar from './components/Navbar';
+import PageContainer from './components/Page';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    AppNavbar,
+    PageContainer,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Page.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    I'm the Page!
+  </div>
+</template>
